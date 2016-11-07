969e751acca5c5aed090b8b5282c2d780631adb2
carloshernando <carlosco96@hotmail.com>
2016-11-07 09:23:51 -0500

    Filter Error solucion mejorada

--- app/src/main/java/com/example/carloshernando/agora/Feed.java
+++ app/src/main/java/com/example/carloshernando/agora/Feed.java
@@ -16,32 +16,7 @@ public class Feed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feed);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-                final ActionBar abar = getSupportActionBar();
-
-                View viewActionBar = getLayoutInflater().inflate(R.layout.feed, null);
-                ActionBar.LayoutParams params = new ActionBar.LayoutParams(//Center the textview in the ActionBar !
-                        ActionBar.LayoutParams.WRAP_CONTENT,
-                        ActionBar.LayoutParams.MATCH_PARENT,
-                        Gravity.CENTER);
-                TextView textviewTitle = (TextView) viewActionBar.findViewById(R.id.actionbar_textview);
-                textviewTitle.setText("Agora");
-                abar.setCustomView(viewActionBar, params);
-                abar.setDisplayShowCustomEnabled(true);
-                abar.setDisplayShowTitleEnabled(false);
-                abar.setDisplayHomeAsUpEnabled(true);
-                             abar.setHomeButtonEnabled(true);
-            }
-        });
     }
 
 }
