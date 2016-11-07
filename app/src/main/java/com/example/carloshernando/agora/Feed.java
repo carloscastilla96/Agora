c5f5972eedaef15917eae889d998bfbbacecf68d
carloshernando <carlosco96@hotmail.com>
2016-11-07 09:20:21 -0500

    Filter Error solucion

--- app/src/main/java/com/example/carloshernando/agora/Feed.java
+++ app/src/main/java/com/example/carloshernando/agora/Feed.java
@@ -3,16 +3,19 @@ package com.example.carloshernando.agora;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
 
 public class Feed extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.feed);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -22,6 +25,21 @@ public class Feed extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                final ActionBar abar = getSupportActionBar();
+
+                View viewActionBar = getLayoutInflater().inflate(R.layout.feed, null);
+                ActionBar.LayoutParams params = new ActionBar.LayoutParams(//Center the textview in the ActionBar !
+                        ActionBar.LayoutParams.WRAP_CONTENT,
+                        ActionBar.LayoutParams.MATCH_PARENT,
+                        Gravity.CENTER);
+                TextView textviewTitle = (TextView) viewActionBar.findViewById(R.id.actionbar_textview);
+                textviewTitle.setText("Agora");
+                abar.setCustomView(viewActionBar, params);
+                abar.setDisplayShowCustomEnabled(true);
+                abar.setDisplayShowTitleEnabled(false);
+                abar.setDisplayHomeAsUpEnabled(true);
+                             abar.setHomeButtonEnabled(true);
             }
         });
     }
