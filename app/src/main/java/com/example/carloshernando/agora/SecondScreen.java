3a4de299377151d407b0a49a94e53887d499a43b
carloshernando <carlosco96@hotmail.com>
2016-11-06 19:18:14 -0500

    Actividad SecondScreen Completa

--- app/src/main/java/com/example/carloshernando/agora/SecondScreen.java
+++ app/src/main/java/com/example/carloshernando/agora/SecondScreen.java
@@ -2,13 +2,28 @@ package com.example.carloshernando.agora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondScreen extends AppCompatActivity {
+
+
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username= (EditText) findViewById(R.id.user);
+        password= (EditText) findViewById(R.id.password);
+
+    }
+
+
+    public void onInicioSesion(View v) {
+
 
     }
 }
