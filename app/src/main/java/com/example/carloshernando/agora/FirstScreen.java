6fe60cfe7561660e3e3a14bbf4cc29c02aee62c2
carloshernando <carlosco96@hotmail.com>
2016-11-06 21:22:16 -0500

    nuevo pull con mmas pantallas

---
+++ app/src/main/java/com/example/carloshernando/agora/FirstScreen.java
@@ -0,0 +1,25 @@
+package com.example.carloshernando.agora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+    }
+
+
+    public void onInicioSesion(View v) {
+
+
+    }
+
+    public void onRegistro(View v) {
+
+
+    }
+}
